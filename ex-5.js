5f130f449132239fd2a929a596d723ea0ac2b055
Pong <pongpong34756@gmail.com>
2024-05-09 15:30:40 +0700

    feat:Primitive value

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalItemPrice = 27000;
+let discount = (totalItemPrice*0.25);
+let totalPrice = totalItemPrice - discount;
+console.log(discount);
+console.log(totalPrice)
