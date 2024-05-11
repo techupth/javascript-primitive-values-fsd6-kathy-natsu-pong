49b08ff3653bd1e1126cf0f79d667ac0d194da5a
KathyKZ <kathykate.sooksutum@gmail.com>
2024-05-11 14:04:52 +0700

    feat; add primitive homework

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
-// Start coding here
+let totalPrice = 27000;
+let discountPercentage = 25;
+let netPrice = totalPrice - (totalPrice * (discountPercentage / 100));
+console.log(netPrice);
